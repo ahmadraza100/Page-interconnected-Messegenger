bd1c71710095f0891e641a2a7c44b2df44fa2603
Ahmad Raza <ahmadrazashafi@gmail.com>
2021-01-18 00:51:10 +0500

    Without Confiuration

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+document.querySelector("#send2").addEventListener("click", function(){
+   var messege = document.getElementById("messege2").value; 
+   document.querySelector("#mb1").innerHTML = messege;
+});
